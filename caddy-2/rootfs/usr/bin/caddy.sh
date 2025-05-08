e5b6ca422be9edebbb5173e474a574a5cf3aed45
seb5594 <seb5594@hotmail.de>
2025-05-08 19:50:08 +0200

    Fix config path

--- caddy-2/rootfs/usr/bin/caddy.sh
+++ caddy-2/rootfs/usr/bin/caddy.sh
@@ -103,7 +103,7 @@ caddy_fmt() {
 mount_extra_files() {
 	for file in $(bashio::config 'mount_files' | jq -r '.[]'); do
 		if [[ -f "$file" ]]; then
-			cp "$file" /etc/caddy/
+			cp "/config/${file}" /etc/caddy/
 			echo "Mounted extra file: $file to /etc/caddy/"
 		else
 			echo "Warning: $file not found!"
